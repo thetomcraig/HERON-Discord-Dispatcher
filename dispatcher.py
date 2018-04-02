6c36e408a6deffb1a358968fa51f70bc4fa5e87d
Tom Craig <thetomcraig@icloud.com>
2018-04-02 08:03:52 -0700

    Working on making bot into a class

--- dispatcher.py
+++ dispatcher.py
@@ -5,53 +5,59 @@ import discord
 
 from local_settings import discord_key
 
-client = discord.Client()
 
-timeout = 5
-all_messages = []
+class Bot():
+
+    def __init__(self, name):
+        self.name = name
+        self.client = discord.Client()
+
+        self.timeout = 5
+        self.all_messages = []
+
+        @self.client.event
+        async def on_ready():
+            print('Logged in as')
+            print(self.client.user.name)
+            print(self.client.user.id)
+            print('------')
+
+        @self.client.event
+        async def on_message(message):
+            """
+            This function will loop, it sends a message,
+            then gets called again when that message gets sent
+            """
+            tmp = await self.client.send_message(message.channel, 'Calculating messages...')
+
+            response = self.get_response(message)
+
+            await self.client.edit_message(tmp, response)
+
+            # Sleep for a random amount of time, then send the message
+            timeout = random.randrange(1, 10)
+            await asyncio.sleep(timeout)
+            print('here')
+            await self.client.send_message(message.channel, response)
+        self.client.run(discord_key)
+
+        self.client.send_message(self.client.channel, 'test')
+
+    def get_response(self, message):
+        """
+        Send the message to the backend
+        The backend will generate a response and save it to the database, then return it
+        """
+        # r = requests.get('API_ENDPOINT')
+        # Do some corner case checking.. is this the first message?
+        #  counter = 0
+        #  tmp = await client.send_message(message.channel, 'Calculating messages...')
+        #  async for log in client.logs_from(message.channel, limit=100):
+        #  if log.author == message.author:
+        #  counter += 1
+        # Maybe look at all the previous messsages?
+        self.all_messages.extend(message.content)
+        return 'test hi'
 
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-
-def get_response(message):
-    """
-    Send the message to the backend
-    The backend will generate a response and save it to the database, then return it
-    """
-    # r = requests.get('API_ENDPOINT')
-    # Do some corner case checking.. is this the first message?
-    #  counter = 0
-    #  tmp = await client.send_message(message.channel, 'Calculating messages...')
-    #  async for log in client.logs_from(message.channel, limit=100):
-    #  if log.author == message.author:
-    #  counter += 1
-    # Maybe look at all the previous messsages?
-    all_messages.extend(message.content)
-    return 'test hi'
-
-
-@client.event
-async def on_message(message):
-    """
-    This function will loop, it sends a message,
-    then gets called again when that message gets sent
-    """
-    tmp = await client.send_message(message.channel, 'Calculating messages...')
-
-    response = get_response(message)
-
-    await client.edit_message(tmp, response)
-
-    # Sleep for a random amount of time, then send the message
-    timeout = random.randrange(1, 10)
-    await asyncio.sleep(timeout)
-    print('here')
-    await client.send_message(message.channel, response)
-
-client.run(discord_key)
+anger_bot = Bot('angry_bot')
