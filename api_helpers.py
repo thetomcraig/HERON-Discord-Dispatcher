0a058acb21914e69da042d735c5df7a3f03cd834
Tom Craig <thetomcraig@icloud.com>
2018-05-27 15:22:59 -0700

    Fix API endpoints, successfully log in and register with HERON

--- api_helpers.py
+++ api_helpers.py
@@ -5,17 +5,11 @@ import requests
 from settings import BOT_ONLINE_ENDPOINT
 
 
-def send_online_message(caller_key, username, conversation_name):
+def send_logged_in_message(caller_key, username, conversation_name):
     post_data = json.dumps({'username': username,
                             'key': caller_key,
                             'conversation_name': conversation_name})
 
     response = requests.post(BOT_ONLINE_ENDPOINT, data=post_data)
-
-    response_string = response.content.decode('utf-8')
-    response_dict = json.loads(response_string)
-
-    print('api helpers')
-    print(response_dict['success'])
 
     return response
--- discord_dispatcher.py
+++ discord_dispatcher.py
@@ -1,15 +1,12 @@
 import asyncio
-import random
-from time import sleep
 import sys
 
 import discord
 
-from api_helpers import send_online_message
+from api_helpers import send_logged_in_message
+from local_settings import conversation_name
 
 timeout = 5
-all_messages = []
-conversation_name = 'general'
 
 
 def run_bot(discord_key):
@@ -19,10 +16,12 @@ def run_bot(discord_key):
 
     @client.event
     async def on_ready():
-        send_online_message(key, client.user.name, conversation_name)
+        send_logged_in_message(key, client.user.name, conversation_name)
 
     async def send_reply(channel, reply):
+        print('sending reply pt 1')
         await client.send_message(channel, reply)
+        print('sending reply pt 2')
 
     client.run(discord_key)
 
