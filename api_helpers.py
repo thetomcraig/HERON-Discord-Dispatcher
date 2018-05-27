e6e56945eaf44c0412372c3c181a24939106eab6
Tom Craig <thetomcraig@icloud.com>
2018-05-27 16:29:41 -0700

    Rework discord logic to check if reply should be sent - look for file in tmp dir

--- api_helpers.py
+++ api_helpers.py
@@ -1,5 +1,5 @@
 import json
-
+import sys
 import requests
 
 from settings import BOT_ONLINE_ENDPOINT
@@ -9,7 +9,9 @@ def send_logged_in_message(caller_key, username, conversation_name):
     post_data = json.dumps({'username': username,
                             'key': caller_key,
                             'conversation_name': conversation_name})
-
-    response = requests.post(BOT_ONLINE_ENDPOINT, data=post_data)
-
-    return response
+    try:
+        response = requests.post(BOT_ONLINE_ENDPOINT, data=post_data)
+        return response
+    except requests.exceptions.RequestException:
+        print('Uable to contact HERON; exiting')
+        sys.exit(1)
--- discord_dispatcher.py
+++ discord_dispatcher.py
@@ -1,29 +1,52 @@
 import asyncio
 import sys
+import os
 
 import discord
 
 from api_helpers import send_logged_in_message
 from local_settings import conversation_name
 
-timeout = 5
-
 
 def run_bot(discord_key):
 
-    client = discord.Client()
     key = discord_key
+    client = discord.Client()
 
     @client.event
     async def on_ready():
+        print('Logged in')
         send_logged_in_message(key, client.user.name, conversation_name)
 
-    async def send_reply(channel, reply):
-        print('sending reply pt 1')
-        await client.send_message(channel, reply)
-        print('sending reply pt 2')
+    @client.event
+    async def on_message(message):
+        check_forever = True
 
-    client.run(discord_key)
+        while(check_forever):
+            # Check the fs to see if message exists
+            reply = check_file_system_for_reply(client.user.name)
+            if reply:
+                await asyncio.sleep(2)
+                await client.send_message(message.channel, reply)
+            else:
+                await asyncio.sleep(20)
+
+    def check_file_system_for_reply(username):
+        reply = None
+        file_name = '/tmp/discord/{}.txt'.format(username)
+        try:
+            with open(file_name, 'r') as f:
+                lines = f.readlines()
+                if len(lines):
+                    reply = lines[0]
+                    os.remove(file_name)
+
+        except Exception as e:
+            print(e)
+
+        return reply
+
+    client.run(key)
 
 
 if __name__ == "__main__":
